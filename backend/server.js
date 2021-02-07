2a9b34911600921564ac294ccf5e915e5d962456
unknown <imoramira@gmail.com>
2021-02-07 20:31:52 +0200

    mongoose
    conrcting to the DB whit mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
-const express = require('express')
+import express  from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config() 
+//call to the mongos 
+connectDB() 
+
 const app = express()
-const products = require ('./data/products')
+
 
 app.get('/',(req,res) => {
     res.send('API is runing')
@@ -13,4 +21,9 @@ app.get('/api/products/:id',(req,res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('server running on port 5000' ))
+const PORT =process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}` )
+    )
