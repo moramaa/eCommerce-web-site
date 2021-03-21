6beafc75c81ddb86b25b271923cdc83b51b09359
unknown <imoramira@gmail.com>
2021-03-21 07:48:03 +0200

    BackEnd registration

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -15,11 +15,12 @@ token = req.headers.authorization.split(' ')[1]
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    console.log(decoded)
-
+    req.user = await User.findById(decoded.id).select('-password')
+    
     next()
 } catch (error) {
-    
+    console.error(error)
+    res.status(401)
 }        }
 
         if(!token){
